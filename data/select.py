ca492e4f4c45ba9fc4f0653a93ae3f145b630343
ARTUR <artur.neves@live.com>
2018-10-11 23:17:01 -0300

    Starting Select Structures

--- data/select.py
+++ data/select.py
@@ -1,10 +1,22 @@
-import core
+from data import core
 from sqlalchemy import select
 
+# Select * from TB_COND
+def select_All_Cond():
+    selCond = select([core.table_cond])
+    for row in selCond.execute():
+        print(row)
+
 # Select Condômino por CPF
-def selectCond(cpf):
+def select_CPF_Cond(cpf):
     selCond = select([core.table_cond]).where(core.table_cond.c.cpf == cpf)
     for row in selCond.execute():
+        print(row)
+
+# Select * from TB_FUNC
+def select_All_Func():
+    selFunc = select([core.table_func])
+    for row in selFunc.execute():
         print(row)
 
 # Select Funcionario por CPF
@@ -13,10 +25,22 @@ def selectFunc(cpf):
     for row in selFunc.execute():
         print(row)
 
+# Select * from TB_TERRENO
+def select_All_Terreno():
+    selTerreno = select([core.table_terreno])
+    for row in selTerreno.execute():
+        print(row)
+
 # Select Terreno por número de lote
 def  selectTerreno(lote):
     selTerreno = select([core.table_terreno]).where(core.table_terreno.c.lote == lote)
     for row in selTerreno.execute():
+        print(row)
+
+# Select * from TB_PrestServ
+def select_All_PrestServ():
+    selPrestServ = select([core.table_prestserv])
+    for row in selPrestServ.execute():
         print(row)
 
 # Select Prestador de Serviço por CPF
@@ -28,7 +52,11 @@ def selectPrestserv(cpf):
 
 # To Debug
 # if __name__ == '__main__':
-    # sel = selectCond("38339663860")
+    # sel = select_All_Cond()
+    # sel = select_CPF_Cond("38339663860")
+    # sel = select_All_Func()
     # sel = selectFunc("40890663971")
+    # sel = select_All_Terreno()
     # sel = selectTerreno(100)
+    # sel = select_All_PrestServ()
     # sel = selectPrestserv('12309812312')
